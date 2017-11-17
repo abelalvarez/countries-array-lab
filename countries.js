350290ccf148a36edc43933d620d159a2c1a8d37
Abel Alvarez <abeldejesusa50344@students.laalliance.org>
2017-11-17 17:40:31 +0000

    Done.

---
+++ countries.js
@@ -0,0 +1,10 @@
+var countries = ["Chad" , "Cuba" , "Greenland" , "Iraq" , "Mali" , "Oman" , "India"
+]
+
+countries.push ("Fiji")
+countries.splice (4, 0, "Iran")
+countries.splice (7,1)
+countries.reverse()
+countries.length
+
+console.log (countries)
